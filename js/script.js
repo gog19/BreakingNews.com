bf42c81a8c30ee93eec9ff308d704168bbfc9f07
gayratyuldoshev-go <devinfinitum0@gmail.com>
2021-02-05 18:15:42 +0500

    Connect with api

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
-let general = $_('.general')
+let general = $_('.form-group')
+let inputValue = $_('.input')
 
-general.addEventListener('click', evt => {
-  fetch('https://newsapi.org/v2/everything?q=bitcoin&apiKey=f8576b990f454e098ec4dc89cc795bc6')
+general.addEventListener('submit', evt => {
+  evt.preventDefault();
+
+  fetch(`https://newsapi.org/v2/everything?q=${inputValue.value}&apiKey=f8576b990f454e098ec4dc89cc795bc6`)
     .then(function (response) {
       return response.json();
     })
